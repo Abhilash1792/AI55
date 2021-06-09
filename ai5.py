b874c637e3de86c36f2298d1c07b586bbf36d32c
ABHILASH_17 <70633358+Abhilash1792@users.noreply.github.com>
2021-06-09 19:00:20 +0530

    Add files via upload

---
+++ ai5.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Best First Search
+
+# In[10]:
+
+
+from queue import PriorityQueue
+v = 14
+graph = [[] for i in range(v)]
+
+def best_first_search(source, target, n):
+    visited = [0] * n
+    pq = PriorityQueue()
+    pq.put((0, source))
+    while pq.empty() == False:
+        u = pq.get()[1]
+        # Displaying the path having lowest cost
+        print(u, end=" ")
+        if u == target:
+            break
+ 
+        for v, c in graph[u]:
+            if visited[v] == 0:
+                visited[v] = 1
+                pq.put((c, v))
+    print()
+
+def addedge(x, y, cost):
+    graph[x].append((y, cost))
+    graph[y].append((x, cost))
+
+addedge(0, 1, 3)
+addedge(0, 2, 6)
+addedge(0, 3, 5)
+addedge(1, 4, 9)
+addedge(1, 5, 8)
+addedge(2, 6, 12)
+addedge(2, 7, 14)
+addedge(3, 8, 7)
+addedge(8, 9, 5)
+addedge(8, 10, 6)
+addedge(9, 11, 1)
+addedge(9, 12, 10)
+addedge(9, 13, 2)
+ 
+source = 0
+target = 9
+print("TSP path of graph: ")
+best_first_search(source, target, v)
+
+
+# # A* algorithm
+
+# In[11]:
+
+
+class Node():
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    open_list = []
+    closed_list = []
+    open_list.append(start_node)
+
+    while len(open_list) > 0:
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+            new_node = Node(current_node, node_position)
+            children.append(new_node)
+
+        for child in children:
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+            open_list.append(child)
+
+maze = [[0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
+
+start = (0, 0)
+end = (0, 3)
+
+path = astar(maze, start, end)
+print("Path in the maze: ",path)
+
+
+# In[ ]:
+
+
+
+
